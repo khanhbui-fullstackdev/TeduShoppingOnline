5b0d5d89c878c732625000a74cd6596cb9c682a8
Yuri-PC\Yuri <bvkhanh0502@gmail.com>
2018-06-21 23:18:57 +0700

    testing angularjs

---
+++ TeduShopingOnline.Web/Scripts/spa/app.module.js
@@ -0,0 +1,13 @@
+﻿/// <reference path="../plugins/angular/angular.min.js" />
+
+// initiate module
+var appModule = angular.module('appModule', []);
+
+// initiate controller
+appModule.controller("myController", myController);
+
+myController.$inject = ['$scope'];
+
+function myController($scope) {
+    $scope.message = "Good morning!";
+}
