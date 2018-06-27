698d93374e6dee69e827005da40b72d24e0d2c5b
Yuri-PC\Yuri <bvkhanh0502@gmail.com>
2018-06-27 22:35:59 +0700

    filter field value in product category

---
+++ TeduShopingOnline.Web/Assets/Admin/app/shared/filters/statusFilter.js
@@ -0,0 +1,11 @@
+﻿/// <reference path="../../../../../scripts/plugins/angular/angular.min.js" />
+
+(function (app) {
+    var statusFilter = app.filter('statusFilter', function () {
+        return function (input) {
+            if (input == true)
+                return 'Active';
+            return 'Inactive';
+        }
+    });
+}(angular.module('common.module')));
